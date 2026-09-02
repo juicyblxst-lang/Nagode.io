0500532e4d5718550a26e81cc350a11852044308
juicyblxst-lang <juicyblxst@gmail.com>
2026-09-02 13:22:04 -0700

    feat: add Nagode application entrypoint

---
+++ backend/src/main/java/io/nagode/NagodeApplication.java
@@ -0,0 +1,11 @@
+package io.nagode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class NagodeApplication {
+  public static void main(String[] args) { SpringApplication.run(NagodeApplication.class, args); }
+}
